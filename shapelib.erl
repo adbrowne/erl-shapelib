ede9d771f914c74ec604cff94f9dc9af1b374056
Andrew Browne <brownie@brownie.com.au>
2011-07-06 23:24:10 +1000

    Completed read code.

--- shapelib.erl
+++ shapelib.erl
@@ -2,12 +2,24 @@
 
 -export([get_shapes/1]).
 
+get_parts(NumParts,Parts) ->
+  [X || <<X:32/little-integer>> <= Parts].
+
+get_points(Points) ->
+  [{X, Y} || <<X:64/little-float,Y:64/little-float>> <= Points].
+
 get_polygon(S, Offset, ContentLength) ->
   {ok, Content} = file:pread(S,Offset,ContentLength * 2),
   io:format("~p~n",[Offset]),
-  ContentBits = ContentLength * 2 * 8 - 32 * 7,
-  <<RecordShapeType:32/little,Xmin:32/little-float,Ymin:32/little-float, Xmax:32/little-float, Ymax:32/little-float,NumParts:32, NumPoints:32,_:ContentBits>> = Content,
- {Xmin,Ymin,Xmax,Ymax}.
+  ContentBits = ContentLength * 2 * 8 - 64 * 4 - 32 * 3,
+  <<RecordShapeType:32/little,Xmin:64/little-float,Ymin:64/little-float, Xmax:64/little-float, Ymax:64/little-float,NumParts:32/little-integer, NumPoints:32/little-integer,PartsAndPoints:ContentBits/binary-unit:1>> = Content,
+  io:format("ContentBits: ~p  NumParts: ~p  NumPoints: ~p~n",[ContentBits, NumParts, NumPoints]),
+  PartBits = NumParts * 32,
+  PointBits = ContentBits - PartBits,
+  <<Parts:PartBits/binary-unit:1,Points:PointBits/binary-unit:1>> = PartsAndPoints,
+  PartList = get_parts(NumParts,Parts), 
+  PointList = get_points(Points),
+  {Xmin,Ymin,Xmax,Ymax,PartList,PointList}.
 
 get_polygons(S,Offset,FileLength) ->
   io:format("~w~n",[Offset]),
@@ -20,7 +32,8 @@ get_polygons(S,Offset,FileLength) ->
 get_shapes(File) ->
   case file:open(File, [read, binary,raw]) of
     {ok, S} ->
-      {ok, <<9994:32/big,_:160,FileLength:32/big,Version:32/little,ShapeType:32/little,Xmin:32/little-float,Ymin:32/little-float,Xmax:32/little-float,Ymax:32/little-float,_:384>>} = file:pread(S, 0, 100),
+      {ok, <<9994:32/big,_:160,FileLength:32/big,Version:32/little,ShapeType:32/little,Xmin:64/little-float,Ymin:64/little-float,Xmax:64/little-float,Ymax:64/little-float,_:(352-96)>>} = file:pread(S, 0, 100),
+      io:format("~p ~p ~p ~p~n",[Xmin,Xmax,Ymin,Ymax]),
       % ensure polygon type
       5 = ShapeType,
       Polygons = get_polygons(S,100,FileLength),
