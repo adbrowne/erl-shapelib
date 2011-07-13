e4f35f5c7196400dd3379df2a35a74cb030a20b1
Andrew Browne <brownie@brownie.com.au>
2011-07-13 22:45:57 +1000

    Converted to printing as I go to save memory

--- shapelib.erl
+++ shapelib.erl
@@ -14,13 +14,13 @@ get_partpoints(PartList, PointList) ->
     [H1, H2 | T] -> 
       Len = H2 - H1,
       Start = H1 + 1,
-      io:format("~p ~p ~p~n",[Start, Len, length(PointList)]), 
+      %io:format("~p ~p ~p~n",[Start, Len, length(PointList)]), 
       [lists:sublist(PointList, Start, Len) | get_partpoints([H2|T], PointList)]
   end.
 
 get_polygon(S, Offset, ContentLength) ->
   {ok, Content} = file:pread(S,Offset,ContentLength * 2),
-  io:format("~p~n",[Offset]),
+  %io:format("~p~n",[Offset]),
   ContentBits = ContentLength * 2 * 8 - 64 * 4 - 32 * 3,
   <<RecordShapeType:32/little,Xmin:64/little-float,Ymin:64/little-float, Xmax:64/little-float, Ymax:64/little-float,NumParts:32/little-integer, NumPoints:32/little-integer,PartsAndPoints:ContentBits/binary-unit:1>> = Content,
   io:format("ContentBits: ~p  NumParts: ~p  NumPoints: ~p~n",[ContentBits, NumParts, NumPoints]),
@@ -33,7 +33,7 @@ get_polygon(S, Offset, ContentLength) ->
   {Xmin,Ymin,Xmax,Ymax,PartList,PartPoints}.
 
 get_polygons(S,Offset,FileLength) ->
-  io:format("~w~n",[Offset]),
+  io:format("~w of ~w~n",[Offset,FileLength]),
   case file:pread(S, Offset, 8) of
     {ok, <<RecordNumber:32/big,ContentLength:32/big>>} -> 
       [{RecordNumber, get_polygon(S,Offset+8,ContentLength)} | get_polygons(S,Offset + ContentLength * 2 + 8,FileLength)];
@@ -54,21 +54,30 @@ get_shapes(File) ->
       error
   end. 
 
-format_points([],notfirst) -> [];
-format_points([{X,Y}|T], notfirst) ->
-  [io_lib:format("L~p,~p",[X,Y])|format_points(T,notfirst)].
+format_points([],_,notfirst) -> ok;
+format_points([{X,Y}|T], FileHandle, notfirst) ->
+  io:format(FileHandle,"L~p,~p",[X,Y]),
+  format_points(T,FileHandle, notfirst),
+  ok.
 
-format_points([{X,Y}|T]) ->
-  [io_lib:format("M~p,~p",[X,Y])|format_points(T,notfirst)].
-get_scaled_string(Part,{Scale,Xmin,Ymin,Height}) ->
+format_points([],_) -> ok;
+format_points([{X,Y}|T],FileHandle) ->
+  io:format(FileHandle,"M~p,~p",[X,Y]),
+  format_points(T,FileHandle,notfirst),
+  ok.
+
+get_scaled_string(Part,{Scale,Xmin,Ymin,Height},FileHandle) ->
   ScaledPoints = [{(X-Xmin)*Scale, Height-(Y-Ymin) * Scale} || {X,Y} <- Part],
-  format_points(ScaledPoints).
+  format_points(ScaledPoints,FileHandle),
+  io:format(".",[]).
   %Output = [lists:map(fun(I) -> {X,Y} = I, [io_lib:format("L~p,~p",[X,Y])] end, ScaledPoints)],
   %Output.
 
-get_scaled_strings_for_polygon(Polygon, ScaleFactors) ->
+get_scaled_strings_for_polygon(Polygon, ScaleFactors,FileHandle) ->
   {_, {Xmin, Ymin, Xmax, Ymax, Parts, Points}} = Polygon,
-  [get_scaled_string(X,ScaleFactors) || X <- Points].
+  io:format(FileHandle,"\"",[]),
+  [get_scaled_string(X,ScaleFactors,FileHandle) || X <- Points],
+  io:format(FileHandle,"\",~n",[]).
 
 write_json(ShapeFile, JsonFile,Width, Height) ->
   Shapes = get_shapes(ShapeFile),
@@ -76,10 +85,8 @@ write_json(ShapeFile, JsonFile,Width, Height) ->
   XScale = Width/(Xmax - Xmin),
   YScale = Height/(Ymax - Ymin),
   Scale = max(XScale,YScale),
-  DrawnPoints = [get_scaled_strings_for_polygon(X,{Scale,Xmin, Ymin, Height}) || X <- Polygons],
-  DrawnPoints,
   {ok, S} = file:open(JsonFile, write),
   io:format(S,"MapPoints = [",[]),
-  [io:format(S, "\"~s\"~n,", [Output]) || Output <- DrawnPoints],
+  [get_scaled_strings_for_polygon(X,{Scale,Xmin, Ymin, Height},S) || X <- Polygons],
   io:format(S,"];",[]),
   file:close(S). 
